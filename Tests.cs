e58d6ea16a21d5e69db814a993ae035f99f88f8d
Roger Serfling <SerflingRogerL@JohnDeere.com>
2021-04-08 12:59:43 -0500

    Init commit

---
+++ Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace HelloWorld
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Can_Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
